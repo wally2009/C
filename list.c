0339f0b380d4fdc3503b0b5dd75f516e84883300
wally2009 <33385495+wally2009@users.noreply.github.com>
2021-07-25 17:24:54 -0400

    Update and rename pr.c to list.c

--- list.c
+++ list.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
-
-
-
 #include<stdlib.h>
 
-/*Create Node*/
+/*A data structure called node with two members : int and a pointer to a another node*/
 struct node 
 	{
 		int x ;
 		struct node *next; 
-
-
 	};
-/*Node definition*/
+
+/*Type defintion */
 typedef struct node node_t; 
 typedef node_t* list; 
 
-
-/*Create a new node */
+/*Create a new node : We expect to receive a node_t* type*/
 node_t* newNode(int x){
 	node_t* tmp = malloc(sizeof(node_t)); 
 	tmp->x = x; 
@@ -27,30 +22,24 @@ node_t* newNode(int x){
 
 /*Add node at the end */
 node_t* addNodeAtTheEnd(node_t* head, int x){
-
-	node_t* tmp;
-	node_t* n = newNode(x); 
+	//Check if the list is empty
 	if(head == NULL) {
 		head = n;
 		return head; 
 	}
-
-
+	
+	node_t* tmp;
+	node_t* n = newNode(x);
 	tmp = head; 
-
-	while(tmp->next != NULL){
-		
+	while(tmp->next != NULL){	
 		tmp = tmp->next; 
-
-		}
+	}
 	tmp->next = n; 
-
 	return head; 
 }
 
 /*Find node*/
 node_t* findNode(node_t* head, int x){
-	
 	node_t* tmp = head; 
 	while(tmp->next != NULL){
 		if(tmp->x == x){
@@ -59,69 +48,46 @@ node_t* findNode(node_t* head, int x){
 		tmp = tmp->next; 
 	}
 	return NULL; 
-
-	}
-
+}
 
 /*add node at the head of hte list. */
-node_t* addNodeAtTheHead(node_t* head, int x)
-	{
-
+node_t* addNodeAtTheHead(node_t* head, int x){
 		node_t* tmp = newNode(x);
 		tmp->next = head;
 		head = tmp;  
 		return head; 
-
-
 	}
 
+/*Delete a node*/
 node_t * deleteNode(node_t* head, int x){
-		node_t* n = newNode(x);
-		node_t* tmp = head; 
-		node_t* driver = head; 
-		node_t* prev ;// = NULL; 
-		while(tmp->x != x)
-			{
+	node_t* n = newNode(x);   //Create a node using the newnode function
+	node_t* tmp = head; 	  //Here tmp is initialzed to the head of the list
+	node_t* prev ; 
+	//This loop will check till the node before the one to be deleted
+	while(tmp->x != x){
+		prev = tmp; 	
+		tmp = tmp->next; 
+	}		
+	prev->next = tmp->next; 	
+	return head; 
+}
 
-				prev = tmp; 	
-				tmp = tmp->next; 
-				
-			
-			}
-			//tmp = tmp->next;
-			prev->next = tmp->next; 
-		/*
-		while(driver  != NULL); 
-			{
-				if(driver->x == x){
-					prev->next = driver->next ; 
-					free(driver); 
-				}
-
-				driver = driver->next; 
-			} */
-		return head; 
-
-	}
-
+/*Insert a node after a given node*/ 
 void   insertNodeAfter( node_t* after, node_t* mynode){
-
-			
 		mynode->next = after->next;
 		after->next = mynode; 		
-
-
-
 	}
+
+
+/*Display a a linklist*/
 void display(node_t* head){
 		
 	node_t* tmp = head; 
 	while(tmp->next != NULL){
-	
 		printf("%d->", tmp->x);
 		tmp = tmp->next; 
-	
 	}
+	
 	if(tmp->next == NULL){
 		printf("%d->NULL", tmp->x); 
 	}
@@ -135,19 +101,22 @@ int main(){
 
 	node_t* head; 
 	node_t* tmp; 	
+	/*For simplicity, we could use a loop to create a list. I chose to add the node manually*/
 	head = addNodeAtTheEnd(head, 10);
-	head = addNodeAtTheEnd(head, 20);
-	 
+	head = addNodeAtTheEnd(head, 20); 
 	head = addNodeAtTheEnd(head, 30);
 	head = addNodeAtTheEnd(head, 40);
 	head = addNodeAtTheEnd(head, 50);
 	head = addNodeAtTheEnd(head, 60);
-	/*
+	
+	/*I want to insert a node at the end of my list*/
 	head = addNodeAtTheHead(head, 100);
 	head = addNodeAtTheHead(head, 150);
 	head = addNodeAtTheHead(head, 200);
-	*/
+	
+	//the list is displayed. 
 	display(head); 
+	
 	tmp = findNode(head, 20); 
 	insertNodeAfter(tmp , newNode(2));
 	printf("After inserting a node :\n"); 
